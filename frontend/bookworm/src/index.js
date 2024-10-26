e675f60f1cf5e50832d1c796ac62cf0be613f462
sanyag77 <69723274+sanyag77@users.noreply.github.com>
2024-10-26 15:38:24 -0400

    Update index.js

--- frontend/bookworm/src/index.js
+++ frontend/bookworm/src/index.js
@@ -10,17 +10,30 @@
 //     <App />
 //   </React.StrictMode>
 // );
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import App from './App';
 
-ReactDOM.render(
-  <Router>
+// ReactDOM.render(
+//   <Router>
+//     <App />
+//   </Router>,
+//   document.getElementById('root')
+// );
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
     <App />
-  </Router>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
